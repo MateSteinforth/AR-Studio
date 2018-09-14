77c95445ae2661a46a99b28368a4e282337cc708
Mate Steinforth <mate1@gmx.net>
2018-09-14 13:40:41 +0200

    Update pointToLineDist.js

--- pointToLineDist.js
+++ pointToLineDist.js
@@ -1,6 +1,6 @@
-var pointToLineDist = function(L0, L1, P0) { // point signal
+var pointToLineDist = function(L0, L1, P0) { // input point signals
 	var v = L1.sub(L0);
-	return P0.distance(L0.add(v.mul(P0.sub(L0).dot(v).div(v.dot(v)))));
+	return P0.distance(L0.add(v.mul(P0.sub(L0).dot(v).div(v.dot(v))))); // returns scalar signal
 };
 
 // adapted from http://geomalgorithms.com/a02-_lines.html
